2978497c736ae23a04e97fd369bc68421f3fc2a0
Ion Bazan <ion.bazan@gmail.com>
2025-01-28 11:30:01 +0800

    Demonstrate skipped test with unexpected output

---
+++ tests/end-to-end/regression/6109/Issue6109Test.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace PHPUnit\TestFixture\Issue6109;
+
+use PHPUnit\Framework\TestCase;
+
+final class Issue6109Test extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        print '*';
+
+        $this->markTestSkipped('This test is failing when running with JUnit Logger');
+    }
+
+    public function testOne(): void
+    {
+        $this->assertTrue(true);
+    }
+}
